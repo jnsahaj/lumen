aeb5ddd02c719be4bbf13011c9d5728ff73527a8
mulla028 <amullagaliev@myseneca.ca>
2024-11-24 20:33:56 -0500

    Refactor ExplainCommand to handle '-' commit SHA by reading from stdin

--- src/command/explain.rs
+++ src/command/explain.rs
@@ -1,8 +1,10 @@
 use async_trait::async_trait;
 use spinoff::{spinners, Color, Spinner};
+use std::io::Read;
 
 use crate::{error::LumenError, git_entity::GitEntity, provider::LumenProvider};
-
+use crate::git_entity::commit::Commit;
+use crate::git_entity::diff::Diff;
 use super::{Command, LumenCommand};
 
 pub struct ExplainCommand {
@@ -29,5 +31,45 @@ impl Command for ExplainCommand {
 
         LumenCommand::print_with_mdcat(result)?;
         Ok(())
+    }
+}
+
+impl ExplainCommand {
+    pub fn new(git_entity: GitEntity, query: Option<String>) -> Result<Self, LumenError> {
+
+        let git_entity = match git_entity {
+            GitEntity::Diff(Diff::WorkingTree { staged, diff }) if diff == "-" => {
+                // Handle Diff with "-" by reading from stdin
+                let mut buffer = String::new();
+                std::io::stdin()
+                    .read_to_string(&mut buffer)
+                    .map_err(LumenError::from)?;
+
+                println!("Replacing '-' with input for Diff: '{}'", buffer.trim());
+
+                GitEntity::Diff(Diff::WorkingTree {
+                    staged,
+                    diff: buffer.trim().to_string(),
+                })
+            }
+
+            GitEntity::Commit(commit_sha) if commit_sha.to_string() == "-" => {
+                // Handle Commit with "-" by reading from stdin
+                let mut buffer = String::new();
+                std::io::stdin()
+                    .read_to_string(&mut buffer)
+                    .map_err(LumenError::from)?;
+
+                println!("Replacing '-' with input for Commit: '{}'", buffer.trim());
+
+                GitEntity::Commit(Commit::new(buffer.trim().to_string())?)
+            }
+            _ => git_entity,
+        };
+
+        Ok(ExplainCommand {
+            git_entity,
+            query,
+        })
     }
 }
