ac6354a2b9767718e9f3bee08286a2f6cebc9e8f
Vinicius Deolindo <git@vini.cat>
2026-07-16 19:08:45 -0300

    nix: a collection of 3 improvements to the flake (#183)
    * nix: format flake with `nixfmt`
    
    * nix: stop using `fenix`
    
    * nix: update flake inputs

--- flake.nix
+++ flake.nix
@@ -4,61 +4,60 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    fenix,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      rustToolchain = fenix.packages.${system}.stable.withComponents [
-        "cargo"
-        "clippy"
-        "rustc"
-        "rustfmt"
-        "rust-src"
-      ];
-      rust-analyzer = fenix.packages.${system}.rust-analyzer;
-    in {
-      packages = {
-        lumen = let
-          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        in
-          pkgs.rustPlatform.buildRustPackage {
-            pname = manifest.name;
-            version = manifest.version;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          lumen =
+            let
+              manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+            in
+            pkgs.rustPlatform.buildRustPackage {
+              pname = manifest.name;
+              version = manifest.version;
 
-            cargoLock.lockFile = ./Cargo.lock;
+              cargoLock.lockFile = ./Cargo.lock;
 
-            src = pkgs.lib.cleanSource ./.;
+              src = pkgs.lib.cleanSource ./.;
 
-            nativeBuildInputs = [pkgs.pkg-config pkgs.perl];
-            buildInputs = [pkgs.openssl];
-            doCheck = false;
-          };
-        default = self.packages.${system}.lumen;
-      };
+              nativeBuildInputs = [
+                pkgs.pkg-config
+                pkgs.perl
+              ];
+              buildInputs = [ pkgs.openssl ];
+              doCheck = false;
+            };
+          default = self.packages.${system}.lumen;
+        };
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = [
-          rustToolchain
-          rust-analyzer
-          pkgs.pkg-config
-          pkgs.perl
-        ];
-        buildInputs = [
-          pkgs.openssl
-        ];
-      };
-    })
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rust-analyzer
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.pkg-config
+            pkgs.perl
+          ];
+          buildInputs = [
+            pkgs.openssl
+          ];
+        };
+      }
+    )
     // {
       overlays.default = final: prev: {
         inherit (self.packages.${final.system}) lumen;
